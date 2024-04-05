c7ed849d5b08a634adaf626351974edcd7439781
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2024-04-05 08:19:52 -0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (55.37%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -4,9 +4,9 @@ public class Solution {
         int res=0;
         for (int i=0; i<32; i++){
             int nBit=0;
-            if (((n>>i)&1)==1){
-                nBit=1<<31-i;
-                res=res|nBit;
+            if (((n>>i)&1)==1){//针对每一位的1
+                nBit=1<<31-i;//补成32位，倒序的每一个1
+                res=res|nBit;//并集or或取到
             }
         }
         return res;
